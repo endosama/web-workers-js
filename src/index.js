be4ba371901a1115a049c9b8b1d7af182ed3a048
Andrea Piras <piras.andrea.developer@gmail.com>
2021-04-22 00:56:25 +0200

    solution

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
-import { compute } from "./compute"
-
 const t0 = performance.now()
 
 function initContent(i) {
-  compute(5)
-  const t1 = performance.now()
-  console.log(`Setting the content ${t1 - t0}`)
-  console.log("Is the element defined:", document.getElementById("content" + i))
-  document.getElementById("content" + i).innerHTML = "Loaded"
+  const worker = new Worker("./worker.js")
+  worker.postMessage("Start")
+  worker.onmessage = (e) => {
+    const t1 = performance.now()
+    console.log(`Received message from worker ${t1 - t0}`)
+    console.log("Is element defined:", document.getElementById("content" + i))
+    document.getElementById("content" + i).innerHTML = `Computation time: ${
+      t1 - t0
+    }`
+  }
 }
+
 function init() {
   initContent(1)
   initContent(2)
