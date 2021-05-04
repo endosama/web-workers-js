fbb547a5cdae70738a1e890e2f86b4b393759711
AndreaPiras <andreapiras@rentalcars.com>
2021-05-04 14:17:21 +0100

    feat(base): wait for document to be ready before executing script

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import { compute } from "./compute"
+import { compute } from "./compute";
 
-const t0 = performance.now()
+const t0 = performance.now();
 
 function initContent(i) {
-  compute(5)
-  const t1 = performance.now()
-  console.log(`Setting the content ${t1 - t0}`)
-  console.log("Is the element defined:", document.getElementById("content" + i))
-  document.getElementById("content" + i).innerHTML = "Loaded"
+  compute(5);
+  const t1 = performance.now();
+  console.log(`Setting the content ${t1 - t0}`);
+  console.log(
+    "Is the element defined:",
+    document.getElementById("content" + i)
+  );
+  document.getElementById("content" + i).innerHTML = "Loaded";
 }
 function init() {
-  initContent(1)
-  initContent(2)
+  initContent(1);
+  initContent(2);
 }
 
-init()
+document.addEventListener("DOMContentLoaded", function (event) {
+  init();
+});
